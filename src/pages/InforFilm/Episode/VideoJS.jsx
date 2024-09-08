f7967f8bdade073f87745f68debd2de60059ea1e
vominhtridz <trilove156@gmail.com>
2024-09-08 18:21:10 +0700

    1

--- src/pages/InforFilm/Episode/VideoJS.jsx
+++ src/pages/InforFilm/Episode/VideoJS.jsx
@@ -75,7 +75,7 @@ const MyVideoPlayer = ({ Episodes, CurrEpisode, loading }) => {
         <iframe
           src={CurrEpisode.link_embed}
           width='100%'
-          className={`lg:h-[450px] max-lg:h-[225px]  ${Enlarge ? "absolute border border-gray-900 shadow max-lg:left-4 max-lg:w-[90%] lg:left-20 top-10  lg:h-[35rem]" : ""}`}
+          className={`lg:h-[450px] max-lg:h-[225px]  ${Enlarge ? "absolute border  border-gray-900 shadow lg:w-[80%] max-lg:left-4 max-lg:w-[90%] lg:left-24 top-10  lg:h-[550px]" : ""}`}
           frameBorder='0'
           allowFullScreen
           title='Video Player'
